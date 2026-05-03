171ccbbd093dfa91e5d0a2ddad89c4baa8f0d318
Vala Prashant <valaprashant9793@gmail.com>
2026-05-03 22:07:35 +0530

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/dart_programing_prectice/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.dart_programing_prectice
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/avgOfThreeNum.dart
@@ -0,0 +1,7 @@
+void main(){
+  int a = 10;
+  int b = 10;
+  int c = 50;
+
+  print((a+b+c)/3);
+}
